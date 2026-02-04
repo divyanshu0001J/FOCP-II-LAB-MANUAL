60b77a2d39fe430c78850669dc847ee412c9ce26
divyanshu0001J <divyanshu25csu069@ncuindia.edu>
2026-02-04 09:15:49 +0530

    Update Q27.cpp

--- Q27.cpp
+++ Q27.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main()
 {
-    int i, n, temp = 0;
-    for (i = 1; i <= 5; i++)
+    int i, n, a[5], temp = 0;
+    for (i = 1, n = 0; i <= 5; i++, n++)
     {
         cout << "enter enter prce of item no. " << i << " = ";
-        cin >> n;
-        if (temp < n)
-            temp = n;
+        cin >> a[n];
+        if (temp < a[n])
+            temp = a[n];
     }
     cout << "Maximum=" << temp;
 }
