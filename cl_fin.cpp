b4d10519ac2a4eb20a0aa54140255a0bb491524c
divyanshu0001J <divyanshu25csu069@ncuindia.edu>
2026-03-02 14:19:09 +0530

    Add files via upload

--- cl_fin.cpp
+++ cl_fin.cpp
@@ -1,50 +1,91 @@
-#includeclude < iostream>
+#include <iostream>
 using namespace std;
+
 class SmartLight
 {
     string brand;
     int brightness;
-    int isOn = 0;
+    int isOn;
 
 public:
+    SmartLight();
+    SmartLight(string b, int br, int on);
+    ~SmartLight();
+
     void input_detail();
     void displayStatus();
     void turnOn();
     void turnOff();
     void increaseBrightness();
     void decreaseBrightness();
-    SmartLight()
-        SmartLight(string b, int br, int on);
 };
+
+SmartLight::SmartLight()
+{
+    brand = "xxxx";
+    brightness = 50;
+    isOn = 0;
+}
+
+SmartLight::SmartLight(string b, int br, int on)
+{
+    brand = b;
+
+    if (br < 0)
+        brightness = 0;
+    else if (br > 100)
+        brightness = 100;
+    else
+        brightness = br;
+
+    isOn = on;
+}
+
+SmartLight::~SmartLight()
+{
+    cout << "SmartLight object destroyed." << endl;
+}
+
 void SmartLight::input_detail()
 {
     cout << "Enter the brand of the smart light: ";
     cin >> brand;
+
     cout << "Enter the brightness level (0-100): ";
     cin >> brightness;
+
     if (brightness < 0)
         brightness = 0;
     else if (brightness > 100)
         brightness = 100;
-    cout << "on or off (1 for on, 0 for off): ";
+
+    cout << "On or Off (1 for on, 0 for off): ";
     cin >> isOn;
 }
+
 void SmartLight::displayStatus()
 {
     cout << "\nBrand: " << brand;
     cout << "\nBrightness: " << brightness;
-    cout << "\nStatus: " << (isOn ? "On" : "Off") << endl;
+
+    if (isOn)
+        cout << "\nStatus: ON" << endl;
+    else
+        cout << "\nStatus: OFF" << endl;
 }
+
 void SmartLight::turnOn()
 {
     isOn = 1;
     cout << "The smart light is now ON." << endl;
 }
+
 void SmartLight::turnOff()
 {
     isOn = 0;
     cout << "The smart light is now OFF." << endl;
 }
+
 void SmartLight::increaseBrightness()
 {
     if (brightness < 100)
@@ -52,6 +93,7 @@ void SmartLight::increaseBrightness()
         brightness += 10;
         if (brightness > 100)
             brightness = 100;
+
         cout << "Brightness increased to " << brightness << "." << endl;
     }
     else
@@ -59,6 +101,7 @@ void SmartLight::increaseBrightness()
         cout << "Brightness is already at maximum." << endl;
     }
 }
+
 void SmartLight::decreaseBrightness()
 {
     if (brightness > 0)
@@ -66,6 +109,7 @@ void SmartLight::decreaseBrightness()
         brightness -= 10;
         if (brightness < 0)
             brightness = 0;
+
         cout << "Brightness decreased to " << brightness << "." << endl;
     }
     else
@@ -73,30 +117,17 @@ void SmartLight::decreaseBrightness()
         cout << "Brightness is already at minimum." << endl;
     }
 }
-SmartLight::SmartLight()
-{
-    brand = "xxxx";
-    brightness = 50;
-    isOn = 0;
-}
-SmartLight::SmartLight(string b, int br, int on)
-{
-    brand = b;
-    brightness = br;
-    if (brightness < 0)
-        brightness = 0;
-    else if (brightness > 100)
-        brightness = 100;
-    isOn = on;
-}
+
 int main()
 {
     SmartLight light1;
+
     light1.input_detail();
     light1.displayStatus();
     light1.turnOn();
     light1.increaseBrightness();
     light1.decreaseBrightness();
     light1.turnOff();
+
     return 0;
 }
